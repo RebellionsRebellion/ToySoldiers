d06551b80e6a603d33fcf47185fe311f00d07a77
Jevin <75545931+JevinLevin@users.noreply.github.com>
2025-11-25 20:53:48 +0000

    fix(player-movement) Enter climbing now requires forward key

--- Assets/Scripts/Player/Movement/States/InputMovement/InputMoveState.cs
+++ Assets/Scripts/Player/Movement/States/InputMovement/InputMoveState.cs
@@ -110,7 +110,7 @@ public abstract class InputMoveState : MovementState
 
     public override void CheckTransitions()
     {
-        if (stateMachine.ClimbingState.CanInitiateClimb() && stateMachine.ClimbingState.CanEnter())
+        if (stateMachine.ClimbingState.CanInitiateClimb() && stateMachine.ClimbingState.CanEnter() && stateMachine.InputController.FrameMove.y > 0f)
         {
             SwitchState(stateMachine.ClimbingState);
         }
