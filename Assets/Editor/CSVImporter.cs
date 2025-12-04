9c6f4d86df9e44aa4d846d7b5882edd4690013ee
Katie <87566534+KatieGames@users.noreply.github.com>
2025-12-04 18:09:03 +0000

    Update CSVImporter.cs

--- Assets/Editor/CSVImporter.cs
+++ Assets/Editor/CSVImporter.cs
@@ -2,11 +2,7 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 
-<<<<<<<< HEAD:Assets/Editor/CSVImporter.cs
-public class CSVImporter : MonoBehaviour
-========
 public class CSVImporters : MonoBehaviour
->>>>>>>> dev:Assets/Editor/CSVImporters.cs
 {
     [MenuItem("Tools/Data Importing/Import Weapons")]
     public static void ImportWeapons()
@@ -34,17 +30,6 @@ public class CSVImporters : MonoBehaviour
         Debug.Log("Weapons updated.");
     }
     
-<<<<<<<< HEAD:Assets/Editor/CSVImporter.cs
-    [MenuItem("Tools/Data Importing/Import Sounds")]
-    public static void ImportSounds()
-    {
-        // find the asset with assetdatabase
-        string[] guids = AssetDatabase.FindAssets("t:SoundTypesSO");
-
-        if (guids.Length == 0)
-        {
-            Debug.LogError("SoundTypes asset not found in project! Please make one before continuing to import");
-========
     [MenuItem("Tools/Data Importing/Import Throwables")]
     public static void ImportThrowables()
     {
@@ -54,32 +39,46 @@ public class CSVImporters : MonoBehaviour
         if (guids.Length == 0)
         {
             Debug.LogError("ThrowableTypes asset not found in project! Please make one before continuing to import");
->>>>>>>> dev:Assets/Editor/CSVImporters.cs
             return;
         }
 
         string path = AssetDatabase.GUIDToAssetPath(guids[0]);
-<<<<<<<< HEAD:Assets/Editor/CSVImporter.cs
-        SoundTypesSO so = AssetDatabase.LoadAssetAtPath<SoundTypesSO>(path);
-
-        // load data from the relevant csv
-        so.SoundTypes = DataLoader.LoadSoundsCSV().ToArray();
-========
         ThrowableTypesSO so = AssetDatabase.LoadAssetAtPath<ThrowableTypesSO>(path);
 
         // load data from the relevant csv
         so.ThrowableTypes = DataLoader.LoadThrowablesCSV().ToArray();
->>>>>>>> dev:Assets/Editor/CSVImporters.cs
 
         // save it to the SO
         EditorUtility.SetDirty(so);
         AssetDatabase.SaveAssets();
         AssetDatabase.Refresh();
 
-<<<<<<<< HEAD:Assets/Editor/CSVImporter.cs
-        Debug.Log("Sounds updated.");
-========
         Debug.Log("Throwables updated.");
->>>>>>>> dev:Assets/Editor/CSVImporters.cs
+    }
+
+    [MenuItem("Tools/Data Importing/Import Sounds")]
+    public static void ImportSounds()
+    {
+        // find the asset with assetdatabase
+        string[] guids = AssetDatabase.FindAssets("t:SoundTypesSO");
+
+        if (guids.Length == 0)
+        {
+            Debug.LogError("SoundTypes asset not found in project! Please make one before continuing to import");
+            return;
+        }
+
+        string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+        SoundTypesSO so = AssetDatabase.LoadAssetAtPath<SoundTypesSO>(path);
+
+        // load data from the relevant csv
+        so.SoundTypes = DataLoader.LoadSoundsCSV().ToArray();
+
+        // save it to the SO
+        EditorUtility.SetDirty(so);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        Debug.Log("Sounds updated.");
     }
 }
