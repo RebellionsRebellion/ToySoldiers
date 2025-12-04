1132d437c0252f857f1513c3ea59558af9b23c0b
Katie <87566534+KatieGames@users.noreply.github.com>
2025-12-04 17:55:26 +0000

    fixed well everything
    fixed soundbank loading, and building, also built it

--- Assets/Scripts/SoundSystem/SoundAnimationEvent.cs
+++ Assets/Scripts/SoundSystem/SoundAnimationEvent.cs
@@ -1,10 +1,12 @@
 using SoundSystem;
+using UnityEditor;
 using UnityEngine;
 
 public class SoundAnimationEvent : BaseAnimationEventBehaviour
 {
     public SoundDataSO soundData;
     private WwisePlayer soundPlayer;
+    private AkGameObj akGameObj;
     private GameObject selectedObject;
 
     protected override void OnEnter(Animator animator)
@@ -35,17 +37,33 @@ public class SoundAnimationEvent : BaseAnimationEventBehaviour
             Object.DestroyImmediate(soundPlayer);
             soundPlayer = null;
         }
+
+        if (akGameObj != null)
+        {
+            Object.DestroyImmediate(akGameObj);
+            akGameObj = null;
+        }
+
         receiver = null;
     }
 
     public void ApplySound()
     {
-        selectedObject = UnityEditor.Selection.activeGameObject;
+        selectedObject = Selection.activeGameObject;
         if (selectedObject == null) return;
 
+        // WwisePlayer
         soundPlayer = selectedObject.GetComponent<WwisePlayer>();
         if (soundPlayer == null)
             soundPlayer = selectedObject.AddComponent<WwisePlayer>();
+
+        // AkGameObj
+        akGameObj = selectedObject.GetComponent<AkGameObj>();
+        if (akGameObj == null)
+            akGameObj = selectedObject.AddComponent<AkGameObj>();
+
+        // disable environment aware
+        akGameObj.isEnvironmentAware = false;
     }
 #endif
 }
